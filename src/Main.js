ae390cf9345f92b6be9a9405543d4d464eee1d4e
Odinlevi <odinlevi@gmail.com>
2020-10-11 23:56:01 +0300

    1/2

--- src/Main.js
+++ src/Main.js
@@ -1,127 +1,266 @@
 "use strict";
 
+function TranslationCubeValue() {
+    var r_x = document.getElementById('translation cube x range');
+    var r_y = document.getElementById('translation cube y range');
+    var r_z = document.getElementById('translation cube z range');
+    var i_x = document.getElementById('translation cube x text');
+    var i_y = document.getElementById('translation cube y text');
+    var i_z = document.getElementById('translation cube z text');
+    i_x.value = r_x.value;
+    i_y.value = r_y.value;
+    i_z.value = r_z.value;
+    return [i_x.value, i_y.value, i_z.value];
+}
+
+function TranslationConeValue() {
+    var r_x = document.getElementById('translation cone x range');
+    var r_y = document.getElementById('translation cone y range');
+    var r_z = document.getElementById('translation cone z range');
+    var i_x = document.getElementById('translation cone x text');
+    var i_y = document.getElementById('translation cone y text');
+    var i_z = document.getElementById('translation cone z text');
+    i_x.value = r_x.value;
+    i_y.value = r_y.value;
+    i_z.value = r_z.value;
+    return [i_x.value, i_y.value, i_z.value];
+}
+
+function TranslationSphereValue() {
+    var r_x = document.getElementById('translation sphere x range');
+    var r_y = document.getElementById('translation sphere y range');
+    var r_z = document.getElementById('translation sphere z range');
+    var i_x = document.getElementById('translation sphere x text');
+    var i_y = document.getElementById('translation sphere y text');
+    var i_z = document.getElementById('translation sphere z text');
+    i_x.value = r_x.value;
+    i_y.value = r_y.value;
+    i_z.value = r_z.value;
+    return [i_x.value, i_y.value, i_z.value];
+}
+
+function RotationCubeValue() {
+    var r_x = document.getElementById('rotation cube x range');
+    var r_y = document.getElementById('rotation cube y range');
+    var r_z = document.getElementById('rotation cube z range');
+    var i_x = document.getElementById('rotation cube x text');
+    var i_y = document.getElementById('rotation cube y text');
+    var i_z = document.getElementById('rotation cube z text');
+    i_x.value = r_x.value;
+    i_y.value = r_y.value;
+    i_z.value = r_z.value;
+    return [i_x.value, i_y.value, i_z.value];
+}
+
+function RotationConeValue() {
+    var r_x = document.getElementById('rotation cone x range');
+    var r_y = document.getElementById('rotation cone y range');
+    var r_z = document.getElementById('rotation cone z range');
+    var i_x = document.getElementById('rotation cone x text');
+    var i_y = document.getElementById('rotation cone y text');
+    var i_z = document.getElementById('rotation cone z text');
+    i_x.value = r_x.value;
+    i_y.value = r_y.value;
+    i_z.value = r_z.value;
+    return [i_x.value, i_y.value, i_z.value];
+}
+
+function RotationSphereValue() {
+    var r_x = document.getElementById('rotation sphere x range');
+    var r_y = document.getElementById('rotation sphere y range');
+    var r_z = document.getElementById('rotation sphere z range');
+    var i_x = document.getElementById('rotation sphere x text');
+    var i_y = document.getElementById('rotation sphere y text');
+    var i_z = document.getElementById('rotation sphere z text');
+    i_x.value = r_x.value;
+    i_y.value = r_y.value;
+    i_z.value = r_z.value;
+    return [i_x.value, i_y.value, i_z.value];
+}
+
+function ScalingCubeValue() {
+    var r = document.getElementById('scaling cube range');
+    var i = document.getElementById('scaling cube text');
+    i.value = r.value;
+    return i.value;
+}
+
+function ScalingConeValue() {
+    var r = document.getElementById('scaling cone range');
+    var i = document.getElementById('scaling cone text');
+    i.value = r.value;
+    return i.value;
+}
+
+function ScalingSphereValue() {
+    var r = document.getElementById('scaling sphere range');
+    var i = document.getElementById('scaling sphere text');
+    i.value = r.value;
+    return i.value;
+}
+
+function AddElementCubeButton() {
+    var sign = document.getElementById('add cube')
+    if (sign.value == " + ") {
+        sign.value = " - ";
+        return ;
+    }
+        sign.value = " + ";
+}
+
+function AddElementCube() {
+    var sign = document.getElementById('add cube')
+    if (sign.value == " + ") {
+        return false;
+    }
+        return true;
+}
+
+function AddElementConeButton() {
+    var sign = document.getElementById('add cone')
+    if (sign.value == " + ") {
+        sign.value = " - ";
+        return ;
+    }
+        sign.value = " + ";
+}
+
+function AddElementCone() {
+    var sign = document.getElementById('add cone')
+    if (sign.value == " + ") {
+        return false;
+    }
+        return true;
+}
+
+function AddElementSphereButton() {
+    var sign = document.getElementById('add sphere')
+    if (sign.value == " + ") {
+        sign.value = " - ";
+        return ;
+    }
+        sign.value = " + ";
+}
+
+function AddElementSphere() {
+    var sign = document.getElementById('add sphere')
+    if (sign.value == " + ") {
+        return false;
+    }
+        return true;
+}
+
+function CreateCubeInfo(gl, programInfo, color, translation) {
+    var object = {
+        programInfo: programInfo,
+        bufferInfo: primitives.createCubeWithVertexColorsBufferInfo(gl, 20),
+        uniforms: {
+            u_colorMult: color,
+            u_matrix: m4.identity(),
+        },
+        translation: translation,
+        shouldBeDrawn: AddElementCube()
+    };
+    console.log(object.bufferInfo);
+    return object;
+}
+
+function CreateConeInfo(gl, programInfo, color, translation) {
+    var object = {
+        programInfo: programInfo,
+        bufferInfo: primitives.createTruncatedConeWithVertexColorsBufferInfo(gl, 10, 0, 20, 20, 1, true, false),
+        uniforms: {
+            u_colorMult: color,
+            u_matrix: m4.identity(),
+        },
+        translation: translation,
+        shouldBeDrawn: AddElementCone()
+    };
+    return object;
+}
+
+function CreateSphereInfo(gl, programInfo, color, translation) {
+    var object = {
+        programInfo: programInfo,
+        bufferInfo: primitives.createSphereWithVertexColorsBufferInfo(gl, 10, 20, 10),
+
+        uniforms: {
+            u_colorMult: color,
+            u_matrix: m4.identity(),
+        },
+        translation: translation,
+        shouldBeDrawn: AddElementSphere()
+    };
+
+    return object;
+}
+
 function main() {
-    // Get A WebGL context
-    /** @type {HTMLCanvasElement} */
+
+    var objectsToDraw = [];
     var canvas = document.querySelector("#canvas");
     var gl = canvas.getContext("webgl");
     if (!gl) {
         return;
     }
 
-    // creates buffers with position, normal, texcoord, and vertex color
-    // data for primitives by calling gl.createBuffer, gl.bindBuffer,
-    // and gl.bufferData
-    const sphereBufferInfo = primitives.createSphereWithVertexColorsBufferInfo(gl, 10, 12, 6);
-    const cubeBufferInfo   = primitives.createCubeWithVertexColorsBufferInfo(gl, 20);
-    const coneBufferInfo   = primitives.createTruncatedConeWithVertexColorsBufferInfo(gl, 10, 0, 20, 12, 1, true, false);
-
-    var shapes = [
-        sphereBufferInfo,
-        cubeBufferInfo,
-        coneBufferInfo,
-    ];
-
-
-    // setup GLSL program
     var programInfo = webglUtils.createProgramInfo(gl, ["vertex-shader-3d", "fragment-shader-3d"]);
 
     function degToRad(d) {
         return d * Math.PI / 180;
     }
 
-    function rand(min, max) {
-        return Math.random() * (max - min) + min;
-    }
-
-    function emod(x, n) {
-        return x >= 0 ? (x % n) : ((n - (-x % n)) % n);
-    }
-
-
     var cameraAngleRadians = degToRad(0);
     var fieldOfViewRadians = degToRad(60);
     var cameraHeight = 50;
 
-    var objectsToDraw = [];
-    var objects = [];
+    // Create info about 3 different figures
+    objectsToDraw.push(CreateCubeInfo(gl, programInfo, [0.5, 1, 0.5, 1], [0, 0, 0]));
+    objectsToDraw.push(CreateConeInfo(gl, programInfo, [0.5, 1, 0.5, 1], [50, 0, 0]));
+    objectsToDraw.push(CreateSphereInfo(gl, programInfo, [0.5, 1, 0.5, 1], [-50, 0, 0]));
 
-    //chroma.hsv=function(h,s,v,a){return new Color(h,s,v,a,"hsv")
-
-
-    // Make infos for each object for each object.
-    var baseHue = rand(0, 360);
-    var numObjects = 200;
-    for (var ii = 0; ii < numObjects; ++ii) {
-        var object = {
-            uniforms: {
-                u_colorMult: [0.5, 1, 0.5, 1],
-                u_matrix: m4.identity(),
-            },
-            translation: [rand(-100, 100), rand(-100, 100), rand(-150, -50)],
-            xRotationSpeed: rand(0.8, 1.2),
-            yRotationSpeed: rand(0.8, 1.2),
-        };
-        objects.push(object);
-        objectsToDraw.push({
-            programInfo: programInfo,
-            bufferInfo: shapes[ii % shapes.length],
-            uniforms: object.uniforms,
-        });
-    }
-
-    function computeMatrix(viewProjectionMatrix, translation, xRotation, yRotation) {
+    function computeMatrix(viewProjectionMatrix, translation, rotation) {
         var matrix = m4.translate(viewProjectionMatrix,
             translation[0],
             translation[1],
             translation[2]);
-        matrix = m4.xRotate(matrix, xRotation);
-        return m4.yRotate(matrix, yRotation);
+        matrix = m4.xRotate(matrix, rotation[0]);
+        matrix = m4.yRotate(matrix, rotation[1])
+        return m4.zRotate(matrix, rotation[2]);
     }
 
     requestAnimationFrame(drawScene);
 
-    // Draw the scene.
-    function drawScene(time) {
-        time *= 0.0005;
-
+    function drawScene() {
         webglUtils.resizeCanvasToDisplaySize(gl.canvas);
 
-        // Tell WebGL how to convert from clip space to pixels
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
         gl.enable(gl.CULL_FACE);
         gl.enable(gl.DEPTH_TEST);
 
-        // Clear the canvas AND the depth buffer.
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        // Compute the projection matrix
         var aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
         var projectionMatrix =
             m4.perspective(fieldOfViewRadians, aspect, 1, 2000);
 
-        // Compute the camera's matrix using look at.
-        var cameraPosition = [0, 0, 100];
+        var cameraPosition = [0, 0, 150];
         var target = [0, 0, 0];
         var up = [0, 1, 0];
         var cameraMatrix = m4.lookAt(cameraPosition, target, up);
 
-        // Make a view matrix from the camera matrix.
         var viewMatrix = m4.inverse(cameraMatrix);
 
         var viewProjectionMatrix = m4.multiply(projectionMatrix, viewMatrix);
 
-        // Compute the matrices for each object.
-        objects.forEach(function(object) {
-            object.uniforms.u_matrix = computeMatrix(
-                viewProjectionMatrix,
-                object.translation,
-                object.xRotationSpeed * time,
-                object.yRotationSpeed * time);
-        });
-
-        // ------ Draw the objects --------
+        objectsToDraw[0].uniforms.u_matrix = computeMatrix(viewProjectionMatrix, TranslationCubeValue(), RotationCubeValue());
+        objectsToDraw[0].shouldBeDrawn = AddElementCube();
+        objectsToDraw[1].uniforms.u_matrix = computeMatrix(viewProjectionMatrix, TranslationConeValue(), RotationConeValue());
+        objectsToDraw[1].shouldBeDrawn = AddElementCone();
+        objectsToDraw[2].uniforms.u_matrix = computeMatrix(viewProjectionMatrix, TranslationSphereValue(), RotationSphereValue());
+        objectsToDraw[2].shouldBeDrawn = AddElementSphere();
+        // Draw the objects
 
         var lastUsedProgramInfo = null;
         var lastUsedBufferInfo = null;
@@ -131,32 +270,27 @@ function main() {
             var bufferInfo = object.bufferInfo;
             var bindBuffers = false;
 
-            if (programInfo !== lastUsedProgramInfo) {
-                lastUsedProgramInfo = programInfo;
-                gl.useProgram(programInfo.program);
+            if (object.shouldBeDrawn) {
+                if (programInfo !== lastUsedProgramInfo) {
+                    lastUsedProgramInfo = programInfo;
+                    gl.useProgram(programInfo.program);
 
-                // We have to rebind buffers when changing programs because we
-                // only bind buffers the program uses. So if 2 programs use the same
-                // bufferInfo but the 1st one uses only positions the when the
-                // we switch to the 2nd one some of the attributes will not be on.
-                bindBuffers = true;
+                    bindBuffers = true;
+                }
+                if (bindBuffers || bufferInfo !== lastUsedBufferInfo) {
+                    lastUsedBufferInfo = bufferInfo;
+                    webglUtils.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+                }
+                webglUtils.setUniforms(programInfo, object.uniforms);
+                gl.drawArrays(gl.TRIANGLES, 0, bufferInfo.numElements);
             }
-
-            // Setup all the needed attributes.
-            if (bindBuffers || bufferInfo !== lastUsedBufferInfo) {
-                lastUsedBufferInfo = bufferInfo;
-                webglUtils.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-            }
-
-            // Set the uniforms.
-            webglUtils.setUniforms(programInfo, object.uniforms);
-
-            // Draw
-            gl.drawArrays(gl.TRIANGLES, 0, bufferInfo.numElements);
         });
+
 
         requestAnimationFrame(drawScene);
     }
 }
 
 main();
+
+
